eed7d7077ebd342c3995c4b724d9f5e651c648d7
Rafael Antonio Chilese <rafaelchilese@yahoo.com.br>
2025-03-07 15:18:38 -0300

    Remove métodos não utilizados e simplifica a classe Faction e NonElector

--- NonElector.py
+++ NonElector.py
@@ -4,21 +4,3 @@ class NonElector:
         self.ideology = ideology
         self.bloc = bloc
         self.information = information  # Lista ou dicionário de informações
-
-def information_gathering_phase(self):
-    print("Fase de coleta de informação:")
-    non_elector1 = NonElector("Cardeal A", "Conservador", "Europa", ["Candidato X é favorito entre conservadores."])
-    self.non_electors = [non_elector1]  # Adicione mais não eleitores
-    while True:
-        print("Escolha um cardeal não eleitor para falar:")
-        for i, non_elector in enumerate(self.non_electors):
-            print(f"{i+1}. {non_elector.name}")
-        print("0. Sair")
-        choice = int(input("Digite o número correspondente: "))
-        if choice == 0:
-            break
-        elif 1 <= choice <= len(self.non_electors):
-            selected_non_elector = self.non_electors[choice-1]
-            print(f"Falando com {selected_non_elector.name}:")
-            for info in selected_non_elector.information:
-                print(info)
